2187ad727b2e05aa4b923846ad827228f9862abe
Yura <Yura.DNG@gmail.com>
2020-11-20 20:59:01 +0200

    custom attributes added

--- src/Web/ViewModels/Gallery/GalleryAddImageVM.cs
+++ src/Web/ViewModels/Gallery/GalleryAddImageVM.cs
@@ -1,11 +1,14 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
+using POC.Web.Helpers.Attributes;
 
 namespace POC.Web.ViewModel
 {
   public class GalleryAddImageVM
   {
     [Required(ErrorMessage = "Необхідно завантажити зображення")]
+    [MaxFileSize(10 * 1024 * 1024)]
+    [AllowedExtensions(new string[] {".jpg", ".png"})]
     public IFormFile Image { get; set; }
   }
 }
--- src/Web/ViewModels/Order/CreateOrderVM.cs
+++ src/Web/ViewModels/Order/CreateOrderVM.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
+using POC.Web.Helpers.Attributes;
 
 namespace POC.Web.ViewModel
 {
@@ -18,6 +19,8 @@ namespace POC.Web.ViewModel
     public string CanvasId { get; set; }
 
     [Required(ErrorMessage = "Необхідно завантажити зображення")]
+    [MaxFileSize(10 * 1024 * 1024)]
+    [AllowedExtensions(new string[] {".jpg", ".png"})]
     public IFormFile Image { get; set; }
   }
 }
